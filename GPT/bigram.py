ec8fe6c2f63baf93fef1699d8b629caa32933ef7
Abhilash Bogavalli <sbog0094@sydney.uni.edu.au>
2026-08-27 19:33:16 +1000

    Self Attention with one Head

--- GPT/bigram.py
+++ GPT/bigram.py
@@ -4,9 +4,9 @@ from torch.nn import functional as F
 
 batch_size = 32
 block_size = 8
-max_iters = 3000
+max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
@@ -54,13 +54,32 @@ def estimate_loss():
         out[split] = losses.mean()
     model.train()
     return out 
+class Head(nn.Module):
+    
+    def __init__(self,head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd,head_size,bias= False) 
+        self.query = nn.Linear(n_embd,head_size,bias= False)
+        self.value = nn.Linear(n_embd,head_size,bias= False)
+        self.register_buffer('tril',torch.tril(torch.ones(block_size,block_size)))
 
+    def forward(self,x):
+        B,T,C = x.shape
+        k = self.key(x) # x would B,block_size,n_embd and k,q,v would be B,block_size,head_size
+        q = self.query(x)
+        wei = q @ k.transpose(-2,-1) * C ** -0.5 #B,block_size,block_size 
+        wei = wei.masked_fill(self.tril[:T,:T] == 0,float('-inf'))
+        wei = F.softmax(wei,dim = -1)
+        v = self.value(x)
+        out = wei @ v # B, block_size, head_size
+        return out 
 class BigramLanguageModel(nn.Module):
     
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size,n_embd)
         self.position_embedding_table = nn.Embedding(block_size,n_embd)
+        self.sa_head = Head(n_embd)
         self.lm_head = nn.Linear(n_embd,vocab_size)
 
     def forward(self,idx,targets = None):
@@ -68,6 +87,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) #(B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T,device=device))
         x = tok_emb + pos_emb
+        x = self.sa_head(x)
         logits = self.lm_head(x)
 
         if targets == None:
@@ -82,7 +102,7 @@ class BigramLanguageModel(nn.Module):
         return logits,loss
     
     def generate(self,idx,max_new_tokens):
-        #idx has shape B,T (say 4, 8)
+        '''idx has shape B,T (say 4, 8)
         for _ in range(max_new_tokens):
             logits,loss = self(idx) # (B,T,C) 
 
@@ -93,7 +113,22 @@ class BigramLanguageModel(nn.Module):
             idx_next = torch.multinomial(probs,num_samples=1) # B,1 - it predicts what the next character is for every batch 
 
             idx = torch.cat((idx,idx_next),dim=1) # add the next coming element to our new idx which would be B,T+1 
-        return idx 
+        return idx '''
+        for _ in range(max_new_tokens):
+
+            idx_cond = idx[:,-block_size:]
+
+            logits, loss = self(idx_cond)
+
+            logits = logits[:,-1,:]
+
+            probs = F.softmax(logits,dim = -1)
+            
+            idx_next = torch.multinomial(probs,num_samples=1)
+
+            idx = torch.cat((idx,idx_next),dim = 1)
+
+        return idx
 
 model = BigramLanguageModel()
 m = model.to(device)
