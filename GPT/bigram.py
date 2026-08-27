07c242e8d54793682a624cd3508b3d1889dfe708
Abhilash Bogavalli <sbog0094@sydney.uni.edu.au>
2026-08-27 20:39:05 +1000

    Layer Norm

--- GPT/bigram.py
+++ GPT/bigram.py
@@ -77,20 +77,25 @@ class MultiHead(nn.Module):
 
     def __init__(self,num_heads,head_size):
         super().__init__()
-
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd,n_embd)
 
     def forward(self,x):
-        return torch.cat([h(x) for h in self.heads],dim = -1)
+        out = torch.cat([h(x) for h in self.heads],dim = -1)
+        out = self.proj(out)
+        return out 
+    
 class FeedForward(nn.Module):
     def __init__(self,n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd,n_embd),
-            nn.ReLU()
+            nn.Linear(n_embd,4*n_embd),
+            nn.ReLU(),
+            nn.Linear(4*n_embd,n_embd)
         )
     def forward(self,x):
         return self.net(x)
+    
 class Block(nn.Module):
     
     def __init__(self, n_embd,n_head):
@@ -98,10 +103,12 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHead(n_head,head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self,x):
-        x = self.sa(x)
-        x = self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x+ self.ffwd(self.ln2(x))
         return x
 
 class BigramLanguageModel(nn.Module):
@@ -113,7 +120,8 @@ class BigramLanguageModel(nn.Module):
         self.blocks = nn.Sequential(
             Block(n_embd, n_head = 4),
             Block(n_embd, n_head = 4),
-            Block(n_embd, n_head = 4)
+            Block(n_embd, n_head = 4),
+            nn.LayerNorm(n_embd),
         )
         self.lm_head = nn.Linear(n_embd,vocab_size)
 
